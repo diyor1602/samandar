19c6999e12a4059596a26c1313f91881a340094e
diyor1602 <abdurakhmanov1602@gmail.com>
2024-09-23 16:26:33 +0500

    Refactor Navbar component to add language descriptions and update styles

--- src/Languages.jsx
+++ src/Languages.jsx
@@ -10,6 +10,10 @@ import poland from "/poland.png";
 import greece from "/greece.png";
 import uae from "/uae.png";
 import russia from "/russia.png";
+import list1 from "/list1.png";
+import list2 from "/list2.png";
+import list3 from "/list3.png";
+import list4 from "/list4.png";
 
 const Languages = () => {
   return (
@@ -33,7 +37,62 @@ const Languages = () => {
             <img src={russia} alt="Russia" />
           </div>
         </div>
-        <div></div>
+        <div className={styles.langugesDescription}>
+          <div className={styles.langugeDescTitle}>Lengua8 - это...</div>
+          <div className={styles.langDescList}>
+            <div className={styles.langDescItem}>
+              <div className={styles.langDescTitle}>
+                1 - Предоставление структурированных курсов
+              </div>
+              <div className={styles.langDescText}>
+                Приложение для изучения языков предлагает структурированные
+                курсы, которые позволяют пользователям последовательно изучать
+                язык от базовых до более сложных уровней. Это помогает
+                пользователям понять логику языка, освоить грамматику, улучшить
+                свой словарный запас и развить навыки говорения, чтения и
+                понимания на слух.
+              </div>
+              <img src={list1} alt="List1" />
+            </div>
+            <div className={styles.langDescItem}>
+              <img src={list2} alt="List2" />
+              <div className={styles.langDescTitle}>
+                2 - Интерактивные упражнения и задания
+              </div>
+              <div className={styles.langDescText}>
+                Приложение предлагает разнообразные интерактивные упражнения и
+                задания, которые способствуют активному и практическому обучению
+                языку. Игровые задания, вопросы-ответы, составление предложений
+                и другие упражнения помогают пользователю закрепить свои знания
+                и навыки.
+              </div>
+            </div>
+            <div className={styles.langDescItem}>
+              <div className={styles.langDescTitle}>
+                3 - Словарный запас и грамматика
+              </div>
+              <div className={styles.langDescText}>
+                Приложение предлагает доступ к обширной базе данных слов и фраз,
+                а также грамматическим правилам. Это помогает пользователям
+                расширить свой словарный запас, изучить правильное использование
+                слов и фраз, а также понять основы грамматики языка.
+              </div>
+              <img src={list3} alt="List3" />
+            </div>
+            <div className={styles.langDescItem}>
+              <img src={list4} alt="List4" />
+              <div className={styles.langDescTitle}>
+                4 - Проверка и коррекция ошибок
+              </div>
+              <div className={styles.langDescText}>
+                Приложение предлагает функцию проверки и коррекции ошибок
+                пользователя в письменной и устной форме. Такой инструмент
+                позволяет пользователям улучшать свои навыки и избегать
+                повторения ошибок, а также получать более точную обратную связь.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
